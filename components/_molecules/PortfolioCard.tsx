5eb981be6dc72dee630121c4d0b870a0903f267a
Jeff Barfield <jeff.barfield@butterfield.com>
2022-11-23 23:46:43 -0500

    added our work

---
+++ components/_molecules/PortfolioCard.tsx
@@ -0,0 +1,34 @@
+import * as React from "react";
+import ButtonBase from "@mui/material/ButtonBase";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+
+export default function PortfolioCard({ project }: any) {
+    return (
+        <ButtonBase focusRipple onClick={() => console.log("boop")}>
+            <Card variant={"outlined"}>
+                <CardMedia
+                    component="img"
+                    height="225"
+                    image={project.image}
+                    alt="green iguana"
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                        {project.name}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        {project.description}
+                    </Typography>
+                </CardContent>
+                {/* <CardActions>
+                <Button size="small">Learn More</Button>
+            </CardActions> */}
+            </Card>
+        </ButtonBase>
+    );
+}
