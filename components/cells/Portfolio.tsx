201f269dd7aaec0c16aa1a641d829f461078f217
Jeff Barfield <barfieldjt@gmail.com>
2023-04-23 11:53:47 -0400

    added project level pagination

--- components/cells/Portfolio.tsx
+++ components/cells/Portfolio.tsx
@@ -7,50 +7,6 @@ import PortfolioCard from "../_molecules/projects/PortfolioCard";
 
 import useProjects from "../../hooks/projects/useProjects";
 
-const projects = [
-	{
-		project_name: "Beamish Beer Taps",
-		project_description:
-			"Custom beer taps draw inspiration from the restaurants beloved history to showcase there house beers.",
-		project_hero_image: "/images/beamish-taps/tap_1.jpg",
-	},
-	{
-		project_name: "Cookie Cutter",
-		project_description:
-			"Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica",
-		project_hero_image:
-			"https://images.unsplash.com/photo-1509460181860-1f17dd8cb0b0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1752&q=80",
-	},
-	{
-		project_name: "Neon One",
-		project_description:
-			"Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica",
-		project_hero_image:
-			"https://images.unsplash.com/photo-1543332164-6e82f355badc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
-	},
-	{
-		project_name: "Neon Sign Two",
-		project_description:
-			"Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica",
-		project_hero_image:
-			"https://images.unsplash.com/photo-1550424844-f7b914439c1b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
-	},
-	{
-		project_name: "Beer Tap",
-		project_description:
-			"Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica",
-		project_hero_image:
-			"https://images.unsplash.com/photo-1620219365994-f443a86ea626?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1742&q=80",
-	},
-	{
-		project_name: "Flower Pot",
-		project_description:
-			"Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica",
-		project_hero_image:
-			"https://images.unsplash.com/photo-1485955900006-10f4d324d411?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1744&q=80",
-	},
-];
-
 const Portfolio = () => {
 	const projects = useProjects();
 
--- pages/projects/[identifier].tsx
+++ pages/projects/[identifier].tsx
@@ -1,45 +1,39 @@
-import { useEffect } from "react";
 import { useRouter } from "next/router";
 
 import parse from "html-react-parser";
 
-import Toolbar from "@mui/material/Toolbar/Toolbar";
 import Container from "@mui/material/Container";
 import Box from "@mui/material/Box";
-import Grid from "@mui/material/Grid";
+
 import Typography from "@mui/material/Typography";
 
-import HeroBanner from "../../components/cells/HeroBanner";
-import About from "../../components/cells/About";
 import Image from "next/image";
-import Divider from "@mui/material/Divider";
 
-import { motion } from "framer-motion";
 import SlideIn from "../../components/_atoms/SlideIn";
 
-import useProject from "../../hooks/projects/useProject";
+import useProjects from "../../hooks/projects/useProjects";
+import Pagination from "@mui/material/Pagination";
 
 const Project = ({ projectData }: any) => {
 	const router = useRouter();
 
-	const slides = [
-		{
-			heroBannerType: "image",
-			imageUrl:
-				"https://images.unsplash.com/photo-1611505908502-5b67e53e3a76?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
-			contentAlignment: "center",
-			textColor: "#fff",
-			title: `Project ${router.query.identifer}`,
-			subtitle: "Where the project case study will live",
-			// buttonText: "Learn More",
-			// buttonLink: "/",
-			// buttonColor: "#194666",
-			// buttonTextColor: "#fff",
-		},
-	];
+	const projects = useProjects();
+
 	if (router.isFallback) {
 		return <div style={{ padding: "150px" }}>Loading...</div>;
 	}
+
+	if (projects.isLoading) {
+		return <div>Loading</div>;
+	}
+
+	const currentPage =
+		projects.data.data.findIndex((d: any) => d.project_id === router.query.identifier) + 1;
+
+	const handleChange = (event: React.ChangeEvent<unknown>, value: number) => {
+		router.push(`/projects/${projects.data.data[value - 1].project_id}`);
+	};
+
 	return (
 		<Container
 			maxWidth={"md"}
@@ -47,9 +41,19 @@ const Project = ({ projectData }: any) => {
 				display: "flex",
 				justifyContent: "center",
 				flexDirection: "column",
-				py: { xs: 12, md: 15 },
+				py: { xs: 9, md: 12 },
 			}}
 		>
+			<SlideIn>
+				<Box display="flex" justifyContent="center" mb={3}>
+					<Pagination
+						count={projects.data.data.length}
+						page={currentPage}
+						shape="rounded"
+						onChange={handleChange}
+					/>
+				</Box>
+			</SlideIn>
 			<SlideIn>
 				<Box
 					sx={{
@@ -71,6 +75,7 @@ const Project = ({ projectData }: any) => {
 					/>
 				</Box>
 			</SlideIn>
+
 			<SlideIn delaySlideIn={0.1}>
 				<Typography variant="h2" component="h1" textAlign={"center"} gutterBottom>
 					{projectData?.data?.project_name}
@@ -90,318 +95,6 @@ const Project = ({ projectData }: any) => {
 					</Box>
 				}
 			</SlideIn>
-			{/* <Divider sx={{ my: 3 }} />
-                <Typography variant="h4" component="h3">
-                    Contents
-                </Typography>
-                <ul>
-                    <li>
-                        <a href="#about">About</a>
-                    </li>
-                    <li>
-                        <a href="#the-goal">The Goal</a>
-                    </li>
-                    <li>
-                        <a href="#challenges">Challenges</a>
-                    </li>
-                    <li>
-                        <a href="#strategy-solution">Strategy + Solution</a>
-                    </li>
-                    <li>
-                        <a href="#final-product">Final Product</a>
-                    </li>
-                    <li>
-                        <a href="#conclusion">Conclusion</a>
-                    </li>
-                </ul> */}
-			{/* <Divider sx={{ my: 3 }} />
-                <Typography variant="h5" component="h3" id={"about"} mb={3}>
-                    About
-                </Typography>
-                <Typography variant="body1" mb={2}>
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                    Dolor repellat eum porro, dolores dolorem praesentium modi
-                    quaerat voluptatem neque doloremque omnis asperiores,
-                    molestias, maiores suscipit tenetur ipsam alias esse maxime!
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Id
-                    sit necessitatibus quibusdam, fuga eligendi placeat vero
-                    fugiat praesentium cum magni repellat dolorem voluptate
-                    facere distinctio animi, repellendus rem tenetur mollitia.
-                </Typography>
-                <Typography variant="body1">
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                    Dolor repellat eum porro, dolores dolorem praesentium modi
-                    quaerat voluptatem neque doloremque omnis asperiores,
-                    molestias, maiores suscipit tenetur ipsam alias esse maxime!
-                </Typography> */}
-			{/* <SlideIn delaySlideIn={0.3}>
-				<>
-					<Divider sx={{ my: 3 }} />
-					<Typography variant="h5" component="h3" id={"the-goal"} mb={3}>
-						Objectives
-					</Typography>
-					<Typography variant="body1">To be successful the project needed to:</Typography>
-					<ul>
-						<li>This is a place to list objectives</li>
-						<li>Another project object could be listed here</li>
-						<li>You could even write a slightly longer objective to go here</li>
-					</ul>
-					<Typography variant="body1">
-						Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor repellat eum
-						porro, dolores dolorem praesentium modi quaerat voluptatem neque doloremque
-						omnis asperiores, molestias, maiores suscipit tenetur ipsam alias esse
-						maxime!
-					</Typography>
-				</>
-			</SlideIn> */}
-			{/* <Divider sx={{ my: 3 }} />
-                <Typography
-                    variant="h5"
-                    component="h3"
-                    id={"challenges"}
-                    mb={3}
-                >
-                    Challenges
-                </Typography>
-                <Typography variant="body1" mb={2}>
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                    Dolor repellat eum porro, dolores dolorem praesentium modi
-                    quaerat voluptatem neque doloremque omnis asperiores,
-                    molestias, maiores suscipit tenetur ipsam alias esse maxime!
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Id
-                    sit necessitatibus quibusdam, fuga eligendi placeat vero
-                    fugiat praesentium cum magni repellat dolorem voluptate
-                    facere distinctio animi, repellendus rem tenetur mollitia.
-                </Typography>
-                <Typography variant="body1" mb={2}>
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                    Dolor repellat eum porro, dolores dolorem praesentium modi
-                    quaerat voluptatem neque doloremque omnis asperiores,
-                    molestias, maiores suscipit tenetur ipsam alias esse maxime!
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Id
-                    sit necessitatibus quibusdam, fuga eligendi placeat vero
-                    fugiat praesentium cum magni repellat dolorem voluptate
-                    facere distinctio animi, repellendus rem tenetur mollitia.
-                </Typography>
-                <Typography variant="body1" mb={2}>
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                    Dolor repellat eum porro, dolores dolorem praesentium modi
-                    quaerat voluptatem neque doloremque omnis asperiores,
-                    molestias, maiores suscipit tenetur ipsam alias esse maxime!
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Id
-                    sit necessitatibus quibusdam, fuga eligendi placeat vero
-                    fugiat praesentium cum magni repellat dolorem voluptate
-                    facere distinctio animi, repellendus rem tenetur mollitia.
-                </Typography>
-                <Grid container spacing={3} mb={2}>
-                    <Grid item xs={4} sx={{ height: 300 }}>
-                        <Box
-                            sx={{
-                                position: "relative",
-                                height: "100%",
-                            }}
-                        >
-                            <Image
-                                src="https://picsum.photos/600/800?random=2"
-                                fill={true}
-                                alt="test"
-                                style={{ objectFit: "cover" }}
-                            />
-                        </Box>
-                    </Grid>
-                    <Grid item xs={4} sx={{ height: 300 }}>
-                        <Box
-                            sx={{
-                                position: "relative",
-                                height: "100%",
-                            }}
-                        >
-                            <Image
-                                src="https://picsum.photos/600/800?random=3"
-                                fill={true}
-                                alt="test"
-                                style={{ objectFit: "cover" }}
-                            />
-                        </Box>
-                    </Grid>
-                    <Grid item xs={4} sx={{ height: 300 }}>
-                        <Box
-                            sx={{
-                                position: "relative",
-                                height: "100%",
-                            }}
-                        >
-                            <Image
-                                src="https://picsum.photos/600/800?random=4"
-                                fill={true}
-                                alt="test"
-                                style={{ objectFit: "cover" }}
-                            />
-                        </Box>
-                    </Grid>
-                </Grid>
-                <Typography variant="body1" mb={2}>
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                    Dolor repellat eum porro, dolores dolorem praesentium modi
-                    quaerat voluptatem neque doloremque omnis asperiores,
-                    molestias, maiores suscipit tenetur ipsam alias esse maxime!
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Id
-                    sit necessitatibus quibusdam, fuga eligendi placeat vero
-                    fugiat praesentium cum magni repellat dolorem voluptate
-                    facere distinctio animi, repellendus rem tenetur mollitia.
-                </Typography>
-                <Typography variant="body1" mb={2}>
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                    Dolor repellat eum porro, dolores dolorem praesentium modi
-                    quaerat voluptatem neque doloremque omnis asperiores,
-                    molestias, maiores suscipit tenetur ipsam alias esse maxime!
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Id
-                    sit necessitatibus quibusdam, fuga eligendi placeat vero
-                    fugiat praesentium cum magni repellat dolorem voluptate
-                    facere distinctio animi, repellendus rem tenetur mollitia.
-                </Typography> */}
-			{/* <SlideIn delaySlideIn={0.4}>
-				<>
-					<Divider sx={{ my: 3 }} />
-					<Typography variant="h5" component="h3" id={"strategy-solution"} mb={3}>
-						Strategy + Solution
-					</Typography>
-					<Typography variant="body1" mb={2}>
-						The primary challenges of this project were ensuring that the taps were
-						strong enough to stand up to daily use in the bar. This was resolved by
-						integrating a threaded metal rod and coupling nut to the inside of the print
-						to provide a solid core to the taps.
-					</Typography>
-					<Typography variant="body1" mb={2}>
-						Pulling from the icon mascot of the Beamish House, the goat brew was given a
-						matching tap which places the Beamish white goat front and centre. For the
-						Beamish House brew a model of the Beamish building was used at the head of
-						the tap. In both cases the Icon Beamish House Pub text runs down the base of
-						the tap.
-					</Typography>
-
-					<Grid container spacing={3}>
-						<Grid item xs={12} md={6} sx={{ height: 600 }}>
-							<Box
-								sx={{
-									position: "relative",
-									height: "100%",
-								}}
-							>
-								<Image
-									src="/images/beamish-taps/tap_1_expanded.jpg"
-									fill={true}
-									alt="test"
-									style={{ objectFit: "cover" }}
-								/>
-							</Box>
-						</Grid>
-						<Grid item xs={12} md={6} sx={{ height: 600 }}>
-							<Box
-								sx={{
-									position: "relative",
-									height: "100%",
-								}}
-							>
-								<Image
-									src="/images/beamish-taps/tap_2_expanded.jpg"
-									fill={true}
-									alt="test"
-									style={{ objectFit: "cover" }}
-								/>
-							</Box>
-						</Grid>
-					</Grid>
-					<Typography variant="body1" mb={2}>
-						Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor repellat eum
-						porro, dolores dolorem praesentium modi quaerat voluptatem neque doloremque
-						omnis asperiores, molestias, maiores suscipit tenetur ipsam alias esse
-						maxime! Lorem ipsum dolor sit amet consectetur adipisicing elit. Id sit
-						necessitatibus quibusdam, fuga eligendi placeat vero fugiat praesentium cum
-						magni repellat dolorem voluptate facere distinctio animi, repellendus rem
-						tenetur mollitia.
-					</Typography>
-				</>
-			</SlideIn>
-			<SlideIn delaySlideIn={0.5}>
-				<>
-					<Divider sx={{ my: 3 }} />
-					<Typography variant="h5" component="h3" id={"final-product"} mb={3}>
-						Final Product
-					</Typography>
-					<Typography variant="body1" mb={2}>
-						Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor repellat eum
-						porro, dolores dolorem praesentium modi quaerat voluptatem neque doloremque
-						omnis asperiores, molestias, maiores suscipit tenetur ipsam alias esse
-						maxime!
-					</Typography>
-					<Grid container spacing={3} mb={2}>
-						<Grid item xs={12} md={6} sx={{ height: 400 }}>
-							<Box
-								sx={{
-									position: "relative",
-									height: "100%",
-								}}
-							>
-								<Image
-									src="https://picsum.photos/600/800?random=7"
-									fill={true}
-									alt="test"
-									style={{ objectFit: "cover" }}
-								/>
-							</Box>
-						</Grid>
-						<Grid item xs={12} md={6} sx={{ height: 400 }}>
-							<Box
-								sx={{
-									position: "relative",
-									height: "100%",
-								}}
-							>
-								<Image
-									src="https://picsum.photos/600/800?random=8"
-									fill={true}
-									alt="test"
-									style={{ objectFit: "cover" }}
-								/>
-							</Box>
-						</Grid>
-					</Grid>
-					<Typography variant="body1" mb={2}>
-						Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor repellat eum
-						porro, dolores dolorem praesentium modi quaerat voluptatem neque doloremque
-						omnis asperiores, molestias, maiores suscipit tenetur ipsam alias esse
-						maxime! Lorem ipsum dolor sit amet consectetur adipisicing elit. Id sit
-						necessitatibus quibusdam, fuga eligendi placeat vero fugiat praesentium cum
-						magni repellat dolorem voluptate facere distinctio animi, repellendus rem
-						tenetur mollitia.
-					</Typography>
-					<Typography variant="body1" mb={2}>
-						Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor repellat eum
-						porro, dolores dolorem praesentium modi quaerat voluptatem neque doloremque
-						omnis asperiores, molestias, maiores suscipit tenetur ipsam alias esse
-						maxime! Lorem ipsum dolor sit amet consectetur adipisicing elit. Id sit
-						necessitatibus quibusdam, fuga eligendi placeat vero fugiat praesentium cum
-						magni repellat dolorem voluptate facere distinctio animi, repellendus rem
-						tenetur mollitia.
-					</Typography>
-				</>
-			</SlideIn> */}
-			{/* <Divider sx={{ my: 3 }} />
-                <Typography
-                variant="h5"
-                    component="h3"
-                    id={"conclusion"}
-                    mb={3}
-                    >
-                    Conclusion
-                    </Typography>
-                    <Typography variant="body1" mb={2}>
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                    Dolor repellat eum porro, dolores dolorem praesentium modi
-                    quaerat voluptatem neque doloremque omnis asperiores,
-                    molestias, maiores suscipit tenetur ipsam alias esse maxime!
-                </Typography> */}
 		</Container>
 	);
 };
